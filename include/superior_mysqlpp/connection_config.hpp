c8531a076073f9095cf264dcdf90ce6a620b6b12
jirislav <jiri.kozlovsky@firma.seznam.cz>
2016-10-27 14:34:18 +0200

    ConnectionConfig: Created static factory method for multithreaded apps
    Changed destructor to virtual
    Config definition not stored in pointers anymore

--- include/superior_mysqlpp/connection_config.hpp
+++ include/superior_mysqlpp/connection_config.hpp
@@ -14,42 +14,27 @@
 namespace SuperiorMySqlpp
 {
     class ConnectionConfig {
-        protected:
-            bool _usingSocket;
+        private:
+            bool _usingSocket = false;
+            bool _hasSsl = true;
 
-            const std::string* _database = nullptr;
-            const std::string* _user = nullptr;
-            const std::string* _password = nullptr;
-            const std::string* _target = nullptr;
+            std::string _database;
+            std::string _user;
+            std::string _password;
+            std::string _target;
 
             std::uint16_t _port;
 
             const struct SslConfiguration* _sslConfig;
-            bool _hasSsl;
 
-        public:
-            ConnectionConfig(const SslConfiguration& sslConfig)
-            {
-                _sslConfig = &sslConfig;
-                _hasSsl = true;
-            }
-
-            ConnectionConfig()
-            {
-                _sslConfig = nullptr;
-                _hasSsl = false;
-            }
-
-            void setTcpConnectionDetails(const std::string& database, const std::string& user, const std::string& password, const std::string& host="localhost",
+            void setTcpConnectionDetails(const std::string database, const std::string& user, const std::string& password, const std::string& host="localhost",
                     std::uint16_t port=3306)
             {
-                _usingSocket = false;
+                _database = database;
+                _user = user;
+                _password = password;
 
-                _database = &database;
-                _user = &user;
-                _password = &password;
-
-                _target = &host;
+                _target = host;
                 _port = port;
             }
 
@@ -58,10 +43,55 @@ namespace SuperiorMySqlpp
             {
                 _usingSocket = true;
 
-                _database = &database;
-                _user = &user;
-                _password = &password;
-                _target = &socket;
+                _database = database;
+                _user = user;
+                _password = password;
+                _target = socket;
+            }
+
+        public:
+            ConnectionConfig(const SslConfiguration& sslConfig) : _sslConfig(&sslConfig), _hasSsl(true) {};
+
+            ConnectionConfig() : _sslConfig(nullptr), _hasSsl(false) {};
+
+            static ConnectionConfig getTcpConnectionConfig(const SslConfiguration* sslConfig, const std::string& database,
+                    const std::string& user, const std::string& password, const std::string& host="localhost", std::uint16_t port=3306)
+            {
+                    ConnectionConfig tcpConfig = ConnectionConfig();
+                    tcpConfig.setTcpConnectionDetails(database, user, password, host, port);
+
+                    tcpConfig._sslConfig = sslConfig;
+
+                    return tcpConfig;
+            }
+
+            static ConnectionConfig getTcpConnectionConfig(const std::string& database, const std::string& user, const std::string& password,
+                    const std::string& host="localhost", std::uint16_t port=3306)
+            {
+                ConnectionConfig tcpConfig = ConnectionConfig::getTcpConnectionConfig(nullptr, database, user, password, host, port);
+                tcpConfig._hasSsl = false;
+
+                return tcpConfig;
+            }
+
+            static ConnectionConfig getSocketConnectionConfig(const SslConfiguration* sslConfig, const std::string& database, 
+                    const std::string& user, const std::string& password, const std::string& socket="/var/run/mysqld/mysqld.sock")
+            {
+                ConnectionConfig socketConfig = ConnectionConfig();
+                socketConfig.setSocketConnectionDetails(database, user, password, socket);
+
+                socketConfig._sslConfig = sslConfig;
+
+                return socketConfig;
+            }
+
+            static ConnectionConfig getSocketConnectionConfig(const std::string& database, const std::string& user, const std::string& password,
+                    const std::string& socket="/var/run/mysqld/mysqld.sock")
+            {
+                ConnectionConfig socketConfig = ConnectionConfig::getSocketConnectionConfig(nullptr, database, user, password, socket);
+                socketConfig._hasSsl = false;
+
+                return socketConfig;
             }
 
             const bool isUsingSocket()
@@ -86,24 +116,24 @@ namespace SuperiorMySqlpp
 
             const std::string& getDatabase()
             {
-                return *(_database);
+                return _database;
             }
 
             const std::string& getUser()
             {
-                return *(_user);
+                return _user;
             }
 
             const std::string& getPassword()
             {
-                return *(_password);
+                return _password;
             }
 
             const std::string& getTarget()
             {
-                return *(_target);
+                return _target;
             }
 
-            ~ConnectionConfig() = default;
+            virtual ~ConnectionConfig() = default;
     };
 }
